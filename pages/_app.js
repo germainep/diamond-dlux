4534775d55a3c7dc326d617c9503a1e8d98900ab
Germaine Pannell <me@germainep.com>
2020-11-24 18:54:16 -0500

    better google analytics implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.styl'
+import React, { Fragment } from 'react'
+import Router from 'next/router'
 
+import * as gtag from 'common/src/lib/gtag'
+
+// Notice how we track page-view when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 function MyApp ( { Component, pageProps } ) {
-  return <Component { ...pageProps } />
+  return (
+      <Fragment>
+        <Component { ...pageProps } />
+      </Fragment>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,6 @@ export default function Home () {
       <div className={ styles.container }>
         <Head>
           <html lang='en'/>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BR8BW1ZEMC"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)};
-            gtag('js', new Date());
-
-            gtag('config', 'G-BR8BW1ZEMC');
-          </script>
           <title>Diamond Luxe | Home</title>
           <link rel="preload"/>
           <link rel="icon" href="/favicon.ico"/>
