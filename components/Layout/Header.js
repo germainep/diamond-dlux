a017180f75136972823441fab09de92f0dd6e792
Germaine P <me@germainep.com>
2022-02-11 11:12:11 -0500

    :recycle: refactor(UI): moved Header into Layout
    Moved Header Component into the Layout Folder to have all Layout Component in the same folder

---
+++ components/Layout/Header.js
@@ -0,0 +1,29 @@
+import NextLink from 'next/link'
+import { Center, Link } from '@chakra-ui/react'
+import Logo from '../common/Logo/Logo'
+import { Box, Flex } from '@chakra-ui/react'
+
+const Header = () => {
+  return (
+    <Flex as={'header'} justifyContent='space-between' bg='primary'>
+      <Center>
+        <Logo width={100} />
+      </Center>
+
+      <Center>
+        <NextLink href='/about' passHref>
+          <Link color='white' fontWeight='bolder' textDecoration='none'>
+            About Us
+          </Link>
+        </NextLink>
+        <NextLink href='/contact' passHref>
+          <Link mx={4} color='white' fontWeight='bolder' textDecoration='none'>
+            Contact Us
+          </Link>
+        </NextLink>
+      </Center>
+    </Flex>
+  )
+}
+
+export default Header
