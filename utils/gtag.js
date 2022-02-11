79c6c31d0ea5f91380ed99f676e40a33c4e9a777
Germaine P <me@germainep.com>
2022-02-11 11:13:17 -0500

    updated google analytics implementation.

--- utils/gtag.js
+++ utils/gtag.js
@@ -1,17 +1,12 @@
-export const GA_TRACKING_ID = "G-BR8BW1ZEMC";
+export const GA_TRACKING_ID = 'G-BR8BW1ZEMC'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url
-  });
-};
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
-  window.gtag("event", action, {
+  window.gtag('event', action, {
     event_category: category,
     event_label: label,
-    value: value
-  });
-};
+    value: value,
+  })
+}
