9d6e7d77beaac83f1566fb75b859c65722f55aa9
Germaine P <me@germainep.com>
2022-02-14 17:05:15 -0500

    fix(UI): removed the booking page from the UI for the time being

--- pages/booking.js
+++ pages/booking.js
@@ -1,14 +1,5 @@
 const Booking = () => {
-  return (
-    <>
-      <iframe
-        src='https://orbisx.ca/app/booknow/VSSlM'
-        style='border:0px #ffffff none; max-width:2000px;'
-        name='OrbisXbookingForm'
-        allowFullScreen
-      />
-    </>
-  )
+  return <></>
 }
 
 export default Booking
